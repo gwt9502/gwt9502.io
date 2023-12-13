7c7bd462e289e921f94aad666f8b92baac696943
guanweitao1 <guanweitao1@jd.com>
2023-12-13 13:14:57 +0800

    add seo

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://199406.xyz/sitemap.xml',
+    host: 'https://199406.xyz',
+  }
+}
